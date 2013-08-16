f059668fd5a18377b1601d46a2deeb1bb51c9b69
Sean Eshbaugh <seaneshbaugh@gmail.com>
2013-08-16 09:40:57 -0500

    Added Ruby #10

---
+++ ruby/10/10.rb
@@ -0,0 +1,67 @@
+def sieve_of_atkin(limit)
+  primes = []
+
+  sieve = Array.new(limit) { false }
+
+  square_root_of_limit = Math.sqrt(limit).to_i
+
+  (1..square_root_of_limit).each do |x|
+    (1..square_root_of_limit).each do |y|
+      n = (4 * x * x) + (y * y)
+
+      if n <= limit && (n % 12 == 1 || n % 12 == 5)
+        sieve[n] = !sieve[n]
+      end
+
+      n = (3 * x * x) + (y * y)
+
+      if n <= limit && n % 12 == 7
+        sieve[n] = !sieve[n]
+      end
+
+      n = (3 * x * x) - (y * y)
+
+      if x > y && n <= limit && n % 12 == 11
+        sieve[n] = !sieve[n]
+      end
+    end
+  end
+
+  sieve[2] = true
+
+  sieve[3] = true
+
+  primes << 2
+
+  primes << 3
+
+  n = 5
+
+  while n <= square_root_of_limit do
+    if sieve[n]
+      i = n * n
+
+      while i < limit do
+        sieve[i] = false
+
+        i += n * n
+      end
+
+      primes << n
+    end
+
+    n += 2
+  end
+
+  while n < limit do
+    if sieve[n]
+      primes << n
+    end
+
+    n += 2
+  end
+
+  primes
+end
+
+puts sieve_of_atkin(2000000).inject(:+)
