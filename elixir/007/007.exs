2d69fb4e711aa4e329d5b3344eac7d976aaf1301
Sean Eshbaugh <seaneshbaugh@gmail.com>
2013-08-20 10:27:44 -0500

    Added Elixir #7

---
+++ elixir/007/007.exs
@@ -0,0 +1,21 @@
+defmodule Euler do
+  def sieve_of_eratosthenes(limit) when is_number(limit) and limit > 1 do
+    sieve_of_eratosthenes(2, round(:math.sqrt(limit)), [], :lists.seq(3, limit, 2))
+  end
+
+  defp sieve_of_eratosthenes(prime, limit, primes, integers) when prime > limit do
+    :lists.reverse([prime | primes]) ++ integers
+  end
+
+  defp sieve_of_eratosthenes(prime, limit, primes, integers) do
+    [new_prime | new_integers] = integers |> Enum.filter(fn x -> rem(x, prime) != 0 end)
+
+    sieve_of_eratosthenes(new_prime, limit, [prime | primes], new_integers)
+  end
+end
+
+n = 10001
+
+limit = trunc((n * :math.log(n)) + (n * (:math.log(:math.log(n)))))
+
+IO.puts Euler.sieve_of_eratosthenes(limit) |> Enum.at(n - 1)
