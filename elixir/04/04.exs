a1ee6910c13b2011a5354a62d0bcfdd9397ee49d
Sean Eshbaugh <seaneshbaugh@gmail.com>
2013-08-20 09:54:51 -0500

    Renumbered Elixir solutions.

--- elixir/04/04.exs
+++
@@ -1,1 +0,0 @@
-IO.puts 100..999 |> Enum.map(fn x -> 100..999 |> Enum.map(fn y -> x * y end) end) |> List.flatten() |> Enum.filter(fn x -> integer_to_list(x) == integer_to_list(x) |> Enum.reverse() end) |> :lists.sort() |> List.last()
--- elixir/06/06.exs
+++
@@ -1,1 +0,0 @@
-IO.puts trunc(:math.pow((1..100 |> Enum.reduce(0, fn (x, sum) -> x + sum end)), 2) - (1..100 |> Enum.reduce(0, fn(x, sum) -> :math.pow(x, 2) + sum end)))
