5dbcd7665f43e23ac65a2cd56b3dcbe2cf8358aa
Sean Eshbaugh <seaneshbaugh@gmail.com>
2013-08-21 22:10:29 -0500

    Added JavaScript #2

---
+++ javascript/002/002.js
@@ -0,0 +1,13 @@
+var fibs;
+
+fibs = [1, 2];
+
+while (fibs.slice(-1)[0] < 4000000) {
+  fibs.push(fibs.slice(-2)[0] + fibs.slice(-1)[0]);
+}
+
+console.log(fibs.filter(function(element) {
+  return element % 2 == 0;
+}).reduce(function(previousValue, currentValue) {
+  return previousValue + currentValue;
+}));
