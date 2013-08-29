3664e2a78a36a03f6a2422fb2e85f96b9ed675f3
Sean Eshbaugh <seaneshbaugh@gmail.com>
2013-08-29 10:15:33 -0500

    Added Clojure #7

---
+++ clojure/007/007.clj
@@ -0,0 +1,18 @@
+(defn sieve_of_eratosthenes
+  ([limit]
+    (sieve_of_eratosthenes 2 (Math/round (Math/sqrt limit)) '() (range 3 (+ limit 1) 2))
+  )
+  ([prime, limit, primes, integers]
+    (if (> prime limit)
+      (concat (reverse (concat (list prime) primes)) integers)
+      (let [new_integers (filter #(not= (mod % prime) 0) integers)]
+        (sieve_of_eratosthenes (first new_integers) limit (concat (list prime) primes) (rest new_integers))
+      )
+    )
+  )
+)
+
+(println (let [n 10001]
+    (nth (sieve_of_eratosthenes (Math/round (+ (* n (Math/log n)) (* (Math/log (Math/log n)) n)))) (- n 1))
+  )
+)
