12eb69c666ba1ca315de4da29b98e476c6682195
Sean Eshbaugh <seaneshbaugh@gmail.com>
2013-09-03 11:57:10 -0500

    Added Ruby #22

---
+++ ruby/022/022.rb
@@ -0,0 +1,7 @@
+contents = File.read('../../files/names.txt')
+
+names = contents.split(',').map { |name| name.gsub('"', '') }.sort
+
+scores = names.map.with_index { |name, index| name.each_byte.map { |letter| letter - 64 }.inject(:+) * (index + 1) }
+
+puts scores.inject(:+)
