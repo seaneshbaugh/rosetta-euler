2aff8c8fc74548442fcd594c8255839f00b1a56c
Sean Eshbaugh <seaneshbaugh@gmail.com>
2013-09-05 17:26:05 -0500

    Added Ruby #39

---
+++ ruby/039/039.rb
@@ -0,0 +1,7 @@
+right_triangles = (1..1000).map { |a| (1..1000).map { |b| { a: a, b: b, c: Math.sqrt((a ** 2) + (b ** 2)) } } }.flatten(1)
+
+integer_right_triangles = right_triangles.select { |t| t[:a] + t[:b] + t[:c].to_i < 1000 && t[:c] == t[:c].floor }
+
+grouped_by_perimeter = integer_right_triangles.group_by { |t| t[:a] + t[:b] + t[:c].to_i }
+
+puts grouped_by_perimeter.max_by { |perimeter, triangles| triangles.length }.first
