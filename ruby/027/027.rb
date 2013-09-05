c2284c960377c00e84edc85fdafafa2d5b59f7da
Sean Eshbaugh <seaneshbaugh@gmail.com>
2013-09-05 15:17:04 -0500

    Fixed buggy Integer#prime? method.

--- ruby/027/027.rb
+++ ruby/027/027.rb
@@ -1,34 +1,34 @@
-class Fixnum
+class Integer
   def prime?
-    if !self.integer?
-      false
-    else
-      lower_primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
+    lower_primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
 
-      if lower_primes.include?(self)
-        return true
-      end
-
-      lower_primes.each do |lower_prime|
-        if self % lower_prime == 0
-          return false
-        end
-      end
-
-      upper_margin = Math.sqrt(self).to_i + 1
-
-      current = 101
-
-      begin
-        if self % current == 0
-          return false
-        end
-
-        current += 2
-      end while current < upper_margin
-
-      true
+    if self < 2
+      return false
     end
+
+    if lower_primes.include?(self)
+      return true
+    end
+
+    lower_primes.each do |lower_prime|
+      if self % lower_prime == 0
+        return false
+      end
+    end
+
+    upper_margin = Math.sqrt(self).to_i + 1
+
+    current = 101
+
+    begin
+      if self % current == 0
+        return false
+      end
+
+      current += 2
+    end while current < upper_margin
+
+    true
   end
 end
 
