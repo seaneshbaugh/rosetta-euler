3b22ba336480b3fc161d50a3b1dec21833a8171f
Sean Eshbaugh <seaneshbaugh@gmail.com>
2013-09-06 15:35:53 -0500

    Added Ruby #42

---
+++ ruby/042/042.rb
@@ -0,0 +1,21 @@
+class Array
+  def append_until(&block)
+    self.each do
+      result = block.yield(self)
+
+      self << result if result
+    end
+  end
+end
+
+contents = File.read('../../files/words.txt')
+
+words = contents.split(',').map { |name| name.gsub('"', '') }.sort
+
+word_values = words.map { |word| word.upcase.codepoints.map { |letter| letter - 64 }.inject(:+) }
+
+max = word_values.max
+
+triangle_numbers = [1].append_until { |t| (((t.length + 1) * (t.length + 2)) / 2) if (((t.length + 1) * (t.length + 2)) / 2) < max }
+
+puts word_values.select { |word_value| triangle_numbers.include?(word_value) }.length
