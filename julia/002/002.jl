8437a0241a3f6a6602c73c9757f9b95472a92814
Sean Eshbaugh <seaneshbaugh@gmail.com>
2013-09-11 11:07:03 -0500

    Added Julia #2

---
+++ julia/002/002.jl
@@ -0,0 +1,7 @@
+fibs = [0, 1]
+
+while fibs[length(fibs)] + fibs[length(fibs) - 1] < 4000000
+  append!(fibs, [fibs[length(fibs)] + fibs[length(fibs) - 1]])
+end
+
+println(sum(filter(x -> mod(x, 2) == 0, fibs)))
