012e95bd81f419cf4ecc34ce7aa8f09d82733234
Sean Eshbaugh <seaneshbaugh@gmail.com>
2013-09-14 19:58:40 -0500

    Added Rust #2

---
+++ rust/002/002.rs
@@ -0,0 +1,20 @@
+fn main() {
+    let mut i = 0;
+    let mut j = 1;
+    let mut k = i + j;
+    let mut sum = 0;
+
+    while k < 4000000 {
+        if k % 2 == 0 {
+            sum += k;
+        }
+
+        i = j;
+
+        j = k;
+
+        k = i + j;
+    }
+
+    println(fmt!("%d", sum));
+}
