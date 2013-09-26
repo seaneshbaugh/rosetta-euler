47fb20244b5c0bc15eb206e6b2a6f15050718091
Sean Eshbaugh <seaneshbaugh@gmail.com>
2013-09-26 14:59:21 -0500

    Added Julia #3

---
+++ julia/003/003.jl
@@ -0,0 +1,27 @@
+function prime_factors(n)
+  found = false
+
+  v = 2
+
+  i = 2
+
+  while i < n - 1 && !found
+    if mod(n, i) == 0
+      found = true
+
+      v = i
+    end
+
+    i += 1
+  end
+
+  if found
+    factors = vcat([v], prime_factors(div(n, v)))
+  else
+    factors = [n]
+  end
+
+  factors
+end
+
+println(max(prime_factors(600851475143)))
