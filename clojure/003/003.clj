06b5e6e2610e55840dbf5890364086122cdeeb3a
Sean Eshbaugh <seaneshbaugh@gmail.com>
2013-09-27 10:26:42 -0500

    Greatly improved Clojure #3

--- clojure/003/003.clj
+++ clojure/003/003.clj
@@ -1,45 +1,15 @@
-(defn is_prime
+(defn prime_factors
   ([x]
-    (if (or (< x 2) (and (= (mod x 2) 0) (not= x 2)))
-      false
-      (let [lower_primes '(2 3 5 7 11 13 17 19 23 29 31 37 41 43 47 53 59 61 67 71 73 79 83 89 97)]
-        (if (< x 101)
-          (= true (some #(= x %) lower_primes))
-          (if (= true (some #(= (mod x %) 0) lower_primes))
-            false
-            (loop [guess 101 limit (Math/ceil (Math/sqrt x))]
-              (if (< limit guess)
-                true
-                (if (= (mod x guess) 0)
-                  false
-                  (recur (+ guess 2) limit)
-                )
-              )
-            )
-          )
+    (loop [n x p 2 a '()]
+      (if (< n (* p p))
+        (cons n a)
+        (if (= (mod n p) 0)
+          (recur (quot n p) p (cons p a))
+          (recur n (+ p 1) a)
         )
       )
     )
   )
 )
 
-(defn max_prime_factor
-  ([x]
-    (let [first_guess (Math/round (Math/sqrt x))]
-      (loop [guess (if (= (mod first_guess 2) 0) (+ first_guess 1) first_guess)]
-        (if (= guess 1)
-          1
-          (if (= (mod x guess) 0)
-            (if (is_prime guess)
-              guess
-              (recur (- guess 2))
-            )
-            (recur (- guess 2))
-          )
-        )
-      )
-    )
-  )
-)
-
-(println (max_prime_factor 600851475143))
+(println (apply max (prime_factors 600851475143)))
