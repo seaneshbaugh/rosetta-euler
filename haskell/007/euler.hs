e331347748b13107066555d0e4281be66858e5ab
Sean Eshbaugh <seaneshbaugh@gmail.com>
2013-10-01 17:18:39 -0500

    Added Haskell #7

---
+++ haskell/007/euler.hs
@@ -0,0 +1,18 @@
+import Data.List
+
+main :: IO ()
+
+sieve :: (Integral a) => a -> a -> [a] -> [a] -> [a]
+sieve prime limit primes integers
+    | prime > limit = sort ((prime:primes) ++ integers)
+    | otherwise     = sieve newPrime limit (prime:primes) newIntegers
+                      where
+                        (newPrime:newIntegers) = filter (\x -> mod x prime /= 0) integers
+
+sieveOfEratosthenes :: (Integral a) => a -> [a]
+sieveOfEratosthenes limit = sieve 2 (round $ sqrt $ fromIntegral $ limit) [] [3, 5..limit]
+
+main = print((sieveOfEratosthenes limit) !! (n - 1))
+       where
+         n = 10001
+         limit = round (((log $ fromIntegral $ n) * (fromIntegral n)) + ((log $ log $ fromIntegral $ n) * (fromIntegral n)))
