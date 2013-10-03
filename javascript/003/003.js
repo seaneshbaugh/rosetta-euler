aad9942b5e1322abc19f870faab910cc10b82638
Sean Eshbaugh <seaneshbaugh@gmail.com>
2013-10-03 12:53:01 -0500

    Improved JavaScript #3

--- javascript/003/003.js
+++ javascript/003/003.js
@@ -1,32 +1,21 @@
 function primeFactors(n) {
-  var found, v, i, result;
+  var v, i;
 
-  found = false;
-
-  v = 2;
+  v = null;
 
   for (i = 2; i < n - 1; i++) {
     if (n % i == 0) {
-      found = true;
-
       v = i;
 
       break;
     }
   }
 
-  result = [];
-
-  if (found) {
-    result.push(v);
-
-    result = result.concat(primeFactors(Math.round(n / v)));
-
+  if (v) {
+    return [v].concat(primeFactors(Math.floor(n / v)));
   } else {
-    result.push(n);
+    return [n];
   }
-
-  return result;
 }
 
-console.log(primeFactors(600851475143).slice(-1)[0]);
+console.log(Math.max.apply(null, primeFactors(600851475143)));
