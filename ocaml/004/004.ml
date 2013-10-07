c6f8b1a66d2a71639d13983b77d8b800dd32decb
Sean Eshbaugh <seaneshbaugh@gmail.com>
2013-10-07 10:31:49 -0500

    Added OCaml #4

---
+++ ocaml/004/004.ml
@@ -0,0 +1,17 @@
+let rec range i j = if i > j then [] else i :: (range (i + 1) j)
+
+let reverse_string s =
+  let l = String.length s in
+  let r = String.create l in
+  let last = l - 1 in
+  for i = 0 to last do
+    let j = last - i in
+    r.[i] <- s.[j];
+  done;
+  (r)
+
+let products = List.concat (List.map (fun x -> List.map (fun y -> x * y) (range 100 999)) (range 1 999))
+
+let palindromes = List.filter (fun x -> (string_of_int x) = (reverse_string (string_of_int x))) products;;
+
+Printf.printf "%d\n" (List.fold_left max (List.hd palindromes) palindromes)
