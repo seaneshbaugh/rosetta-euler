78797b7a979e3fa1816c3a84f83f2c1185608e06
Sean Eshbaugh <seaneshbaugh@gmail.com>
2013-10-18 09:47:48 -0500

    Added Python #14

---
+++ python/014/014.py
@@ -0,0 +1,25 @@
+import operator
+
+def collatz(n):
+    if n <= 1:
+        return 1
+    elif n % 2 == 1:
+        return (3 * n) + 1
+    else:
+        return n / 2
+
+chain_lengths = {}
+
+for start in range(1, 1000001):
+    length = 1
+
+    n = start
+
+    while n > 1:
+        n = collatz(n)
+
+        length += 1
+
+    chain_lengths[start] = length
+
+print(map(lambda x: x[0], sorted(chain_lengths.iteritems(), key = operator.itemgetter(1)))[-1:][0])
