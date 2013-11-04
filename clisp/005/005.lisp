d6b66bb055033b39b8263e9e311f7057e1199fc0
Sean Eshbaugh <seaneshbaugh@gmail.com>
2013-11-04 08:27:48 -0600

    Added Common Lisp #5

---
+++ clisp/005/005.lisp
@@ -0,0 +1,30 @@
+(defun prime_factors (x)
+  (labels ((pf (n p)
+    (if (< n (* p p))
+      (list n)
+      (if (= (rem n p) 0)
+        (cons p (pf (/ n p) p))
+        (pf n (+ p 1))))))
+    (pf x 2)))
+
+(defun delete-first (i l)
+  (cond
+    ((null l)
+      '())
+    ((= i (car l))
+      (cdr l))
+    (t
+      (cons (car l) (delete-first i (cdr l))))))
+
+(defun overlap (a b)
+  (sort (append a (labels ((o (x y)
+    (if (null x)
+      y
+      (o (cdr x) (delete-first (car x) y)))))
+    (o a b))) #'<))
+
+(print
+  (reduce #'*
+    (reduce  #'overlap
+      (loop for i from 1 to 20
+        collect (prime_factors i)))))
