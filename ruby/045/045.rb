3cd8a7252629f2c3e01034d5f3aea1da7dfd6476
Sean Eshbaugh <seaneshbaugh@gmail.com>
2013-11-05 09:14:22 -0600

    Added Ruby #45

--- ruby/045/045.rb
+++ ruby/045/045.rb
@@ -5,54 +5,12 @@ class Integer
     (self * (self + 1)) / 2
   end
 
-  def triangular?
-    if self > 0
-      n (Math.sqrt((8.0 * self) + 1.0) - 1.0) / 2.0
-
-      if n.floor == n
-        n
-      else
-        false
-      end
-    else
-      false
-    end
-  end
-
   def pentagon
     (self * ((3 * self) - 1)) / 2
   end
 
-  def pentagonal?
-    if self > 0
-      n = (Math.sqrt((24.0 * self) + 1.0) + 1.0) / 6.0
-
-      if n.floor == n
-        n
-      else
-        false
-      end
-    else
-      false
-    end
-  end
-
   def hexagon
     self * ((2 * self) - 1)
-  end
-
-  def hexagonal?
-    if self > 0
-      n = (Math.sqrt((8.0 * self) + 1.0) + 1.0) / 4.0
-
-      if n.floor == n
-        n
-      else
-        false
-      end
-    else
-      false
-    end
   end
 end
 
