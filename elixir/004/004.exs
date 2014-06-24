15f27c36040bdd4e45132d148bad60990bedca20
Sean Eshbaugh <seaneshbaugh@gmail.com>
2014-06-24 08:22:48 -0500

    Updated Elixir #4

--- elixir/004/004.exs
+++ elixir/004/004.exs
@@ -1,7 +1,7 @@
 IO.puts 100..999
 |> Enum.map(fn x -> 100..999 |> Enum.map(fn y -> x * y end) end)
 |> List.flatten
-|> Enum.filter(fn x -> integer_to_list(x) == integer_to_list(x) |> Enum.reverse end)
+|> Enum.filter(fn x -> Integer.to_char_list(x) == Integer.to_char_list(x) |> Enum.reverse end)
 |> :lists.sort
 |> List.last
 |> to_string
