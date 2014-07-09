da1f19fab11caf29ad8bc5944a77a14b5e1482bb
Sean Eshbaugh <seaneshbaugh@gmail.com>
2014-07-09 08:57:41 -0500

    Fixed prime? in Ruby

--- ruby/035/035.rb
+++ ruby/035/035.rb
@@ -3,7 +3,7 @@ class Integer
     if !self.integer?
       false
     else
-      lower_primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
+      lower_primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101]
 
       if lower_primes.include?(self)
         return true
