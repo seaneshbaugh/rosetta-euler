8d8566ee8c0f4e4b17b2b97fb3f35c0cae9263d9
Sean Eshbaugh <seaneshbaugh@gmail.com>
2014-09-14 23:59:27 -0500

    Updated Elixir #16

--- elixir/016/016.exs
+++ elixir/016/016.exs
@@ -1,8 +1,7 @@
 IO.puts :math.pow(2, 1000)
 |> round
-|> integer_to_list
-|> String.from_char_list!
+|> Integer.to_string
 |> String.codepoints
-|> Enum.map(&(String.to_integer(&1) |> elem(0)))
+|> Enum.map(&(String.to_integer(&1)))
 |> Enum.reduce(0, &(&1 + &2))
 |> to_string
