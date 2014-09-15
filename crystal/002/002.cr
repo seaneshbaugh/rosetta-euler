694fec8eb32aa59bc6a4154eed725c0c489c1322
Sean Eshbaugh <seaneshbaugh@gmail.com>
2014-09-14 20:54:55 -0500

    Added Crystal #2

---
+++ crystal/002/002.cr
@@ -0,0 +1,9 @@
+fibonacci_numbers = [0, 1]
+
+loop do
+  break if fibonacci_numbers[-1] + fibonacci_numbers[-2] > 4000000
+
+  fibonacci_numbers << fibonacci_numbers[-1] + fibonacci_numbers[-2]
+end
+
+puts fibonacci_numbers.select { |n| n.even? }.inject { |acc, i| acc + i }
