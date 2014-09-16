bb542de846ea8b386b7604a354d00fd552577158
Sean Eshbaugh <seaneshbaugh@gmail.com>
2014-09-15 23:03:22 -0500

    Updated Elixir #22

--- elixir/022/022.exs
+++ elixir/022/022.exs
@@ -2,7 +2,7 @@
 
 names = contents |> String.split(",") |> Enum.map(fn name -> name |> String.replace("\"", "") end) |> Enum.sort
 
-scores = names |> Enum.with_index |> Enum.map(fn {name, index} -> (name |> String.to_char_list! |> Enum.map(fn letter -> letter - 64 end) |> Enum.reduce(0, &(&1 + &2))) * (index + 1) end)
+scores = names |> Enum.with_index |> Enum.map(fn {name, index} -> (name |> String.to_char_list |> Enum.map(fn letter -> letter - 64 end) |> Enum.reduce(0, &(&1 + &2))) * (index + 1) end)
 
 IO.puts scores
 |> Enum.reduce(0, &(&1 + &2))
