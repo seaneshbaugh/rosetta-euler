2eee494a3dbfaeaded4ebffbc1bac048a1461587
Sean Eshbaugh <seaneshbaugh@gmail.com>
2014-09-15 23:00:12 -0500

    Updated Elixir #20

--- elixir/020/020.exs
+++ elixir/020/020.exs
@@ -2,6 +2,6 @@ IO.puts 1..100
 |> Enum.reduce(1, &(&1 * &2))
 |> to_string
 |> String.codepoints
-|> Enum.map(fn digit -> digit |> binary_to_integer end)
+|> Enum.map(fn digit -> digit |> String.to_integer end)
 |> Enum.reduce(0, &(&1 + &2))
 |> to_string
