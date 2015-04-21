f12330415542b32b59018982eb5a2b29a83b76ce
Sean Eshbaugh <seaneshbaugh@gmail.com>
2015-04-21 09:50:17 -0500

    Updated Rust #6

--- rust/006/006.rs
+++ rust/006/006.rs
@@ -1,3 +1,3 @@
 fn main() {
-    println(fmt!("%s", ((std::num::pow(range(1u64, 101u64).fold(0, |a, b| a + b) as f64, 2.0) as u64) - range(1u64, 101u64).map(|x| x * x).fold(0, |a, b| a + b)).to_str()));
+    println!("{}", (1u64 .. 101u64).collect::<Vec<u64>>().iter().fold(0, |a, b| a + b).pow(2) - (1u64 .. 101u64).collect::<Vec<u64>>().iter().map(|x| x * x).fold(0, |a, b| a + b));
 }
