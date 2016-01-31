ced6b09e245fc2cec240248c04d63d6f7571a287
Sean Eshbaugh <seaneshbaugh@gmail.com>
2016-01-31 17:51:07 -0600

    Added Nim #5

---
+++ nim/005/euler.nim
@@ -0,0 +1,54 @@
+from sequtils import `concat`, `foldr`
+
+proc prime_factors(n: int64): seq[int64] =
+  var found = false
+
+  var v = 2
+
+  var i = 2
+
+  while i < n - 1 and found == false:
+    if n mod i == 0:
+      found = true
+
+      v = i
+
+    i = i + 1
+
+  result = @[]
+
+  if found:
+    add(result, v)
+
+    result = concat(result, prime_factors(n div v))
+  else:
+    add(result, n)
+
+  return
+
+proc overlap(a: seq[int64], b: seq[int64]): seq[int64] =
+  var c = a
+
+  for i in b:
+    let index = find(c, i)
+
+    if index >= 0:
+      delete(c, index)
+
+  result = concat(c, b)
+
+  return
+
+var factors: seq[int64]
+
+factors = @[]
+
+for i in 1..20:
+  factors = overlap(prime_factors(i), factors)
+
+var product = 1i64
+
+for i in factors:
+  product = product * i
+
+echo(product)
