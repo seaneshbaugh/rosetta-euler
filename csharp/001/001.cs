dd6386f7fca2c4d817fb1fca42dcb379ece38b88
Sean Eshbaugh <seaneshbaugh@gmail.com>
2016-04-18 14:32:06 -0400

    Fixed range bug in C# #1

--- csharp/001/001.cs
+++ csharp/001/001.cs
@@ -4,6 +4,6 @@ using System.Linq;
 
 public class Euler {
 	static public void Main() {
-		Console.WriteLine("{0}", Enumerable.Range(1, 1000).Where(x => x % 3 == 0 || x % 5 == 0).Sum());
+		Console.WriteLine("{0}", Enumerable.Range(1, 999).Where(x => x % 3 == 0 || x % 5 == 0).Sum());
 	}
 }
