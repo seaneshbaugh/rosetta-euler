77177696d56898891ba2b0aba9743902261db3d7
Sean Eshbaugh <seaneshbaugh@gmail.com>
2016-04-19 14:58:39 -0500

    Added tests for Go

---
+++ test/rosetta_euler/go_test.exs
@@ -0,0 +1,28 @@
+Code.require_file "../rosetta_euler_test.exs", __DIR__
+Code.require_file "../support/answers.exs", __DIR__
+
+defmodule RosettaEuler.GoTest do
+  use ExUnit.Case, async: true
+
+  import RosettaEuler.Test, only: [rosetta_euler_tests: 2]
+  import RosettaEuler.Answers, only: [answer_for: 1]
+
+  setup context do
+    {:ok, [test_number: context[:test_number]]}
+  end
+
+  def run_go_solution(number) do
+    name = number |> to_string |> String.rjust(3, ?0)
+
+    System.cmd("go", ["run", name <> ".go"], cd: Path.join("go", name))
+  end
+
+  rosetta_euler_tests 1..8 do
+    @tag test_number: test_number
+    test "Go #" <> to_string(test_number), %{test_number: test_number} do
+      answer = answer_for(test_number) <> "\n"
+
+      {^answer, 0} = run_go_solution(test_number)
+    end
+  end
+end
