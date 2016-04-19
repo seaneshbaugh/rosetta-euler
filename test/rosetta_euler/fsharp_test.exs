3ea00469511afeb3e0f65b19c2f8fd4283f08db6
Sean Eshbaugh <seaneshbaugh@gmail.com>
2016-04-19 13:11:52 -0500

    Added tests for F#

---
+++ test/rosetta_euler/fsharp_test.exs
@@ -0,0 +1,36 @@
+Code.require_file "../rosetta_euler_test.exs", __DIR__
+Code.require_file "../support/answers.exs", __DIR__
+
+defmodule RosettaEuler.FSharpTest do
+  use ExUnit.Case, async: true
+
+  import RosettaEuler.Test, only: [rosetta_euler_tests: 2]
+  import RosettaEuler.Answers, only: [answer_for: 1]
+
+  setup context do
+    {:ok, [test_number: context[:test_number]]}
+  end
+
+  def run_fsharp_solution(number) do
+    name = number |> to_string |> String.rjust(3, ?0)
+
+    path = Path.join("fsharp", name)
+
+    {_, 0} = System.cmd("fsharpc", [name <> ".fs"], cd: path)
+
+    {pwd, 0} = System.cmd("pwd", [], cd: path)
+
+    executable_path = Path.join(String.strip(pwd), name <> ".exe")
+
+    System.cmd("mono", [executable_path], cd: path)
+  end
+
+  rosetta_euler_tests 1..7 do
+    @tag test_number: test_number
+    test "C# #" <> to_string(test_number), %{test_number: test_number} do
+      answer = answer_for(test_number) <> "\n"
+
+      {^answer, 0} = run_fsharp_solution(test_number)
+    end
+  end
+end
