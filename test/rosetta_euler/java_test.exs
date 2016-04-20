d131014e116069416925eea7e06a1002397880c5
Sean Eshbaugh <seaneshbaugh@gmail.com>
2016-04-19 23:31:34 -0500

    Added tests for Java

---
+++ test/rosetta_euler/java_test.exs
@@ -0,0 +1,32 @@
+Code.require_file "../rosetta_euler_test.exs", __DIR__
+Code.require_file "../support/answers.exs", __DIR__
+
+defmodule RosettaEuler.JavaTest do
+  use ExUnit.Case, async: true
+
+  import RosettaEuler.Test, only: [rosetta_euler_tests: 2]
+  import RosettaEuler.Answers, only: [answer_for: 1]
+
+  setup context do
+    {:ok, [test_number: context[:test_number]]}
+  end
+
+  def run_java_solution(number) do
+    name = number |> to_string |> String.rjust(3, ?0)
+
+    path = Path.join("java", name)
+
+    {_, 0} = System.cmd("javac", ["Euler.java"], cd: path)
+
+    System.cmd("java", ["Euler"], cd: path)
+  end
+
+  rosetta_euler_tests 1..6 do
+    @tag test_number: test_number
+    test "Java #" <> to_string(test_number), %{test_number: test_number} do
+      answer = answer_for(test_number) <> "\n"
+
+      {^answer, 0} = run_java_solution(test_number)
+    end
+  end
+end
