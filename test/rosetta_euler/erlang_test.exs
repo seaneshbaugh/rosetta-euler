83df142ee4b7c38868de74d942f94d3b87536800
Sean Eshbaugh <seaneshbaugh@gmail.com>
2016-04-19 23:14:06 -0500

    Added tests for Erlang

---
+++ test/rosetta_euler/erlang_test.exs
@@ -0,0 +1,28 @@
+Code.require_file "../rosetta_euler_test.exs", __DIR__
+Code.require_file "../support/answers.exs", __DIR__
+
+defmodule RosettaEuler.ErlangTest do
+  use ExUnit.Case, async: true
+
+  import RosettaEuler.Test, only: [rosetta_euler_tests: 2]
+  import RosettaEuler.Answers, only: [answer_for: 1]
+
+  setup context do
+    {:ok, [test_number: context[:test_number]]}
+  end
+
+  def run_erlang_solution(number) do
+    name = number |> to_string |> String.rjust(3, ?0)
+
+    System.cmd("escript", ["euler.erl"], cd: Path.join("erlang", name))
+  end
+
+  rosetta_euler_tests 1..10 do
+    @tag test_number: test_number
+    test "Erlang #" <> to_string(test_number), %{test_number: test_number} do
+      answer = answer_for(test_number) <> "\n"
+
+      {^answer, 0} = run_erlang_solution(test_number)
+    end
+  end
+end
