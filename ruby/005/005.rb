2665172fb2e7a1b0e26d03d421b89cb41e33524c
Sean Eshbaugh <seaneshbaugh@gmail.com>
2019-02-17 02:05:36 -0600

    Improved Ruby #5

--- ruby/005/005.rb
+++ ruby/005/005.rb
@@ -1,4 +1,4 @@
-class Fixnum
+class Integer
   def prime_factors
     v = (2..self - 1).detect { |i| self % i == 0 }
 
@@ -11,21 +11,13 @@ class Array
     clone = self.clone
 
     a.each do |n|
-      if clone.index(n)
-        clone.delete_at(clone.index(n))
-      end
+      index = clone.index(n)
+
+      clone.delete_at(index) if index
     end
 
     (a + clone).sort
   end
 end
 
-prime_factor_lists = (1..20).to_a.map { |i| i.prime_factors }
-
-factors = []
-
-prime_factor_lists.each do |prime_factor_list|
-  factors = factors.overlap(prime_factor_list)
-end
-
-puts factors.inject(:*)
+puts (1..20).map(&:prime_factors).inject([]) { |prime_factor_list, factors| factors.overlap(prime_factor_list) }.inject(:*)
