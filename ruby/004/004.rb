da7c4dd018953742d2cf99ca5aed5495b86aaf8a
Sean Eshbaugh <seaneshbaugh@gmail.com>
2019-02-17 01:47:15 -0600

    Use max instead of sort.last

--- ruby/004/004.rb
+++ ruby/004/004.rb
@@ -1,1 +1,1 @@
-puts (100..999).to_a.map { |x| (100..999).to_a.map { |y| x * y } }.flatten.select { |x| x.to_s == x.to_s.reverse }.sort.last
+puts (100..999).to_a.map { |x| (100..999).to_a.map { |y| x * y } }.flatten.select { |x| x.to_s == x.to_s.reverse }.max
