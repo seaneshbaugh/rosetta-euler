e8f76bdfae93e2cc14842be2f696c27a3478d7bb
Sean Eshbaugh <seaneshbaugh@gmail.com>
2019-02-18 12:33:16 -0600

    Improved formatting for Ruby #7

--- ruby/007/007.rb
+++ ruby/007/007.rb
@@ -9,21 +9,15 @@ def sieve_of_atkin(limit)
     (1..square_root_of_limit).each do |y|
       n = (4 * x * x) + (y * y)
 
-      if n <= limit && (n % 12 == 1 || n % 12 == 5)
-        sieve[n] = !sieve[n]
-      end
+      sieve[n] = !sieve[n] if n <= limit && (n % 12 == 1 || n % 12 == 5)
 
       n = (3 * x * x) + (y * y)
 
-      if n <= limit && n % 12 == 7
-        sieve[n] = !sieve[n]
-      end
+      sieve[n] = !sieve[n] if n <= limit && n % 12 == 7
 
       n = (3 * x * x) - (y * y)
 
-      if x > y && n <= limit && n % 12 == 11
-        sieve[n] = !sieve[n]
-      end
+      sieve[n] = !sieve[n] if x > y && n <= limit && n % 12 == 11
     end
   end
 
@@ -37,11 +31,11 @@ def sieve_of_atkin(limit)
 
   n = 5
 
-  while n <= square_root_of_limit do
+  while n <= square_root_of_limit
     if sieve[n]
       i = n * n
 
-      while i < limit do
+      while i < limit
         sieve[i] = false
 
         i += n * n
@@ -53,10 +47,8 @@ def sieve_of_atkin(limit)
     n += 2
   end
 
-  while n < limit do
-    if sieve[n]
-      primes << n
-    end
+  while n < limit
+    primes << n if sieve[n]
 
     n += 2
   end
@@ -66,6 +58,6 @@ end
 
 n = 10001
 
-limit = ((n * Math.log(n)) + (n * (Math.log(Math.log(n))))).to_i
+limit = ((n * Math.log(n)) + (n * Math.log(Math.log(n)))).to_i
 
 puts sieve_of_atkin(limit)[n - 1]
