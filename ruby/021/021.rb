df556ecb78da1c55cdadce7ec7232bbdb3bfd678
Sean Eshbaugh <seaneshbaugh@gmail.com>
2019-02-18 14:13:38 -0600

    Updated Ruby #21

--- ruby/021/021.rb
+++ ruby/021/021.rb
@@ -1,4 +1,4 @@
-class Numeric
+class Integer
   def prime_factors
     v = (2..self - 1).detect { |i| self % i == 0 }
 
@@ -6,19 +6,19 @@ class Numeric
   end
 
   def factors
-    primes, powers = self.prime_factors.group_by { |i| i }.map { |k, v| [k, v.length] }.transpose
+    primes, factor_powers = prime_factors.group_by { |i| i }.map { |k, v| [k, v.length] }.transpose
 
-    exponents = powers.map { |i| (0..i).to_a }
+    exponents = factor_powers.map { |i| (0..i).to_a }
 
     divisors = exponents.shift.product(*exponents).map do |powers|
-      primes.zip(powers).map { |prime, power| prime ** power }.inject(:*)
+      primes.zip(powers).map { |prime, power| prime**power }.inject(:*)
     end
 
     divisors.sort.map { |divisor| [divisor, self / divisor] }
   end
 
   def proper_divisors
-    self.factors.flatten.uniq.reject { |factor| factor == self }.sort
+    factors.flatten.uniq.reject { |factor| factor == self }.sort
   end
 end
 
@@ -33,9 +33,7 @@ amicable_numbers = []
 sums_of_proper_divisors.each do |value, sum_of_proper_divisors|
   possible_amicable_number = sum_of_proper_divisors.proper_divisors.inject(:+)
 
-  if possible_amicable_number == value && sum_of_proper_divisors != possible_amicable_number
-    amicable_numbers << [sum_of_proper_divisors, possible_amicable_number].sort
-  end
+  amicable_numbers << [sum_of_proper_divisors, possible_amicable_number].sort if possible_amicable_number == value && sum_of_proper_divisors != possible_amicable_number
 end
 
 puts amicable_numbers.flatten.uniq.inject(:+)
