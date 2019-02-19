eb5a0115464bacca4a4ed353826053987bbd148f
Sean Eshbaugh <seaneshbaugh@gmail.com>
2019-02-19 15:08:41 -0600

    Updated Ruby #28

--- ruby/028/028.rb
+++ ruby/028/028.rb
@@ -1,7 +1,7 @@
 def spiral_corners(length_of_side)
   raise ArgumentError unless length_of_side.integer? && length_of_side > 0
 
-  last = length_of_side ** 2
+  last = length_of_side**2
 
   [last, last - (length_of_side - 1), last - ((length_of_side - 1) * 2), last - ((length_of_side - 1) * 3)]
 end
