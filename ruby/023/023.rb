f132d17975f145ca2397715aa277f326c0742320
Sean Eshbaugh <seaneshbaugh@gmail.com>
2019-02-19 14:13:26 -0600

    Updated Ruby #23

--- ruby/023/023.rb
+++ ruby/023/023.rb
@@ -1,4 +1,4 @@
-class Numeric
+class Integer
   def prime_factors
     v = (2..self - 1).detect { |i| self % i == 0 }
 
@@ -6,28 +6,28 @@ class Numeric
   end
 
   def factors
-    primes, powers = self.prime_factors.group_by { |i| i }.map { |k, v| [k, v.length] }.transpose
+    primes, factor_powers = prime_factors.group_by { |i| i }.map { |k, v| [k, v.length] }.transpose
 
-    exponents = powers.map { |i| (0..i).to_a }
+    exponents = factor_powers.map { |i| (0..i).to_a }
 
     divisors = exponents.shift.product(*exponents).map do |powers|
-      primes.zip(powers).map { |prime, power| prime ** power }.inject(:*)
+      primes.zip(powers).map { |prime, power| prime**power }.inject(:*)
     end
 
     divisors.sort.map { |divisor| [divisor, self / divisor] }
   end
 
   def proper_divisors
-    self.factors.flatten.uniq.reject { |factor| factor == self }.sort
+    factors.flatten.uniq.reject { |factor| factor == self }.sort
   end
 
   def abundant?
-    self < self.proper_divisors.inject(:+)
+    self < proper_divisors.inject(:+)
   end
 end
 
 known_limit = 28123
 
-abundant_numbers = (12..known_limit).select { |i| i.abundant? }
+abundant_numbers = (12..known_limit).select(&:abundant?)
 
-puts (1..known_limit).select { |i| (abundant_numbers.map { |abundant_number| i - abundant_number if abundant_number < i }.compact & abundant_numbers).length == 0 }.inject(:+)
+puts (1..known_limit).select { |i| (abundant_numbers.map { |abundant_number| i - abundant_number if abundant_number < i }.compact & abundant_numbers).empty? }.inject(:+)
