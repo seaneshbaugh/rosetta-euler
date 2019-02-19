37d507c3ca42633658a851709f2e49b94d6ffcf0
Sean Eshbaugh <seaneshbaugh@gmail.com>
2019-02-19 14:10:36 -0600

    Updated Ruby #22

--- ruby/022/022.rb
+++ ruby/022/022.rb
@@ -1,6 +1,6 @@
 contents = File.read(File.expand_path(File.join(__dir__, '..', '..', 'files', 'names.txt')))
 
-names = contents.split(',').map { |name| name.gsub('"', '') }.sort
+names = contents.split(',').map { |name| name.delete('"') }.sort
 
 scores = names.map.with_index { |name, index| name.each_byte.map { |letter| letter - 64 }.inject(:+) * (index + 1) }
 
