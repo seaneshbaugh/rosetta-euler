4cda9b7aa04451a0866211174a317db327b96371
Sean Eshbaugh <seaneshbaugh@gmail.com>
2019-02-19 15:11:27 -0600

    Updated Ruby #30

--- ruby/030/030.rb
+++ ruby/030/030.rb
@@ -2,12 +2,12 @@ class Integer
   def sum_of_nth_power_of_digits?(n)
     raise ArgumentError unless n.integer? && n > 0
 
-    self.to_s.split(//).map { |digit| digit.to_i ** n }.inject(:+) == self
+    to_s.split('').map { |digit| digit.to_i**n }.inject(:+) == self
   end
 end
 
 n = 5
 
-maximum = (9 ** n) * n
+maximum = (9**n) * n
 
 puts (10..maximum).select { |x| x.sum_of_nth_power_of_digits?(n) }.inject(:+)
