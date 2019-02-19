9c993c93938f65b998c320c0ef8bce935b2a445c
Sean Eshbaugh <seaneshbaugh@gmail.com>
2019-02-19 15:15:43 -0600

    Updated Ruby #32

--- ruby/032/032.rb
+++ ruby/032/032.rb
@@ -1,7 +1,7 @@
-identities = (2..99).map { |x| ((x > 9 ? 123 : 1234)..(10000 / x)).map { |y| { x: x, y: y, p: x * y } } }.flatten(1)
+identities = (2..99).flat_map { |x| ((x > 9 ? 123 : 1234)..(10000 / x)).map { |y| { x: x, y: y, p: x * y } } }
 
-pandigital_identities = identities.select{ |i| [i[:x], i[:y], i[:p]].join.split(//).sort.join == '123456789' }
+pandigital_identities = identities.select { |i| [i[:x], i[:y], i[:p]].join.split('').sort.join == '123456789' }
 
-unique_pandigital_identities = pandigital_identities.uniq{ |i| i[:p] }
+unique_pandigital_identities = pandigital_identities.uniq { |i| i[:p] }
 
 puts unique_pandigital_identities.map { |i| i[:p] }.inject(:+)
