f14966889887a82e1a40f44d2e0bd1a5bacf3f86
Sean Eshbaugh <seaneshbaugh@gmail.com>
2019-02-19 15:07:01 -0600

    Improvied Ruby #27

--- ruby/027/027.rb
+++ ruby/027/027.rb
@@ -2,31 +2,25 @@ class Integer
   def prime?
     lower_primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101]
 
-    if self < 2
-      return false
-    end
+    return false if self < 2
 
-    if lower_primes.include?(self)
-      return true
-    end
+    return true if lower_primes.include?(self)
 
     lower_primes.each do |lower_prime|
-      if self % lower_prime == 0
-        return false
-      end
+      return false if self % lower_prime == 0
     end
 
     upper_margin = Math.sqrt(self).to_i + 1
 
     current = 101
 
-    begin
-      if self % current == 0
-        return false
-      end
+    loop do
+      return false if self % current == 0
 
       current += 2
-    end while current < upper_margin
+
+      break unless current < upper_margin
+    end
 
     true
   end
@@ -38,18 +32,16 @@ consecutive_primes = {}
   (-999..999).each do |b|
     n = 0
 
-    while true
-      if (((n ** 2) + (a * n) + b).abs).prime?
-        n += 1
-      else
-        break
-      end
+    loop do
+      break unless ((n**2) + (a * n) + b).abs.prime?
+
+      n += 1
     end
 
     consecutive_primes[[a, b]] = n
   end
 end
 
-max_consecutive_primes = consecutive_primes.sort_by { |k, v| v }.last
+max_consecutive_primes = consecutive_primes.max_by { |_k, v| v }
 
 puts max_consecutive_primes.first[0] * max_consecutive_primes.first[1]
