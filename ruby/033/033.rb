14e8c4545f00f02ee736975445404f23622bbf1c
Sean Eshbaugh <seaneshbaugh@gmail.com>
2019-02-20 15:47:22 -0600

    Updated Ruby #33

--- ruby/033/033.rb
+++ ruby/033/033.rb
@@ -9,34 +9,32 @@ digit_canceling_fractions = []
     common = false
 
     n_digits.each do |digit|
-      if d_digits.include?(digit) && digit != '0'
-        common = digit
+      next unless d_digits.include?(digit) && digit != '0'
 
-        n_digits.delete(common)
+      common = digit
 
-        d_digits.delete(common)
+      n_digits.delete(common)
 
-        break
-      end
+      d_digits.delete(common)
+
+      break
     end
 
-    if common
-      n = n.to_f
+    next unless common
 
-      d = d.to_f
+    n = n.to_f
 
-      q1 = n / d
+    d = d.to_f
 
-      x = n_digits.join.to_f
+    q1 = n / d
 
-      y = d_digits.join.to_f
+    x = n_digits.join.to_f
 
-      q2 = x / y
+    y = d_digits.join.to_f
 
-      if q1 == q2
-        digit_canceling_fractions << [n.to_i, d.to_i, x.to_i, y.to_i]
-      end
-    end
+    q2 = x / y
+
+    digit_canceling_fractions << [n.to_i, d.to_i, x.to_i, y.to_i] if q1 == q2
   end
 end
 
