0525914079bcb92235ea74f19104e2cdaf6c899c
Sean Eshbaugh <seaneshbaugh@gmail.com>
2019-02-25 13:46:06 -0600

    Updated Ruby #58

--- ruby/058/058.rb
+++ ruby/058/058.rb
@@ -2,31 +2,25 @@ class Integer
   def prime?
     lower_primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101]
 
-    if self < 2
-      return false
-    end
+    return false if self < 2
 
-    if lower_primes.include?(self)
-      return true
-    end
+    return true if lower_primes.include?(self)
 
     lower_primes.each do |lower_prime|
-      if self % lower_prime == 0
-        return false
-      end
+      return false if self % lower_prime == 0
     end
 
     upper_margin = Math.sqrt(self).to_i + 1
 
-    current = 101
+    current = 103
 
-    begin
-      if self % current == 0
-        return false
-      end
+    loop do
+      return false if self % current == 0
 
       current += 2
-    end while current < upper_margin
+
+      break unless current < upper_margin
+    end
 
     true
   end
@@ -46,9 +40,7 @@ loop do
   3.times do
     c += side_length
 
-    if c.prime?
-      number_of_primes += 1
-    end
+    number_of_primes += 1 if c.prime?
   end
 
   c += side_length
