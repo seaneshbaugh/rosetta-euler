eb9fe2a58b11ff4e1c699c8bff18f2b868cb136a
Sean Eshbaugh <seaneshbaugh@gmail.com>
2019-02-25 13:36:07 -0600

    Updated Ruby #49

--- ruby/049/049.rb
+++ ruby/049/049.rb
@@ -9,21 +9,15 @@ def sieve_of_atkin(limit)
     (1..square_root_of_limit).each do |y|
       n = (4 * x * x) + (y * y)
 
-      if n <= limit && (n % 12 == 1 || n % 12 == 5)
-        sieve[n] = !sieve[n]
-      end
+      sieve[n] = !sieve[n] if n <= limit && (n % 12 == 1 || n % 12 == 5)
 
       n = (3 * x * x) + (y * y)
 
-      if n <= limit && n % 12 == 7
-        sieve[n] = !sieve[n]
-      end
+      sieve[n] = !sieve[n] if n <= limit && n % 12 == 7
 
       n = (3 * x * x) - (y * y)
 
-      if x > y && n <= limit && n % 12 == 11
-        sieve[n] = !sieve[n]
-      end
+      sieve[n] = !sieve[n] if x > y && n <= limit && n % 12 == 11
     end
   end
 
@@ -37,11 +31,11 @@ def sieve_of_atkin(limit)
 
   n = 5
 
-  while n <= square_root_of_limit do
+  while n <= square_root_of_limit
     if sieve[n]
       i = n * n
 
-      while i < limit do
+      while i < limit
         sieve[i] = false
 
         i += n * n
@@ -53,10 +47,8 @@ def sieve_of_atkin(limit)
     n += 2
   end
 
-  while n < limit do
-    if sieve[n]
-      primes << n
-    end
+  while n < limit
+    primes << n if sieve[n]
 
     n += 2
   end
@@ -66,14 +58,14 @@ end
 
 class Array
   def evenly_spaced?
-    self.each_cons(2).to_a.map { |pair| (pair.first - pair.last).abs }.uniq.length == 1
+    each_cons(2).to_a.map { |pair| (pair.first - pair.last).abs }.uniq.length == 1
   end
 end
 
-primes = sieve_of_atkin(10000)
+all_primes = sieve_of_atkin(10000)
 
-primes.reject! { |prime| prime.to_s.length != 4 }
+primes.select! { |prime| prime.to_s.length == 4 }
 
-groups = primes.group_by { |prime| prime.to_s.split('').sort }.select { |digits, primes| primes.length >=3 }
+groups = all_primes.group_by { |prime| prime.to_s.split('').sort }.select { |_digits, primes| primes.length >= 3 }
 
-puts groups.map { |digits, primes| primes.permutation(3).to_a.select { |primes| primes.sort.evenly_spaced? } }.map { |primes| primes.flatten.uniq }.uniq.reject { |primes| primes.empty? }.reject { |primes| primes.first == 1487 }.flatten.join
+puts groups.map { |_digits, primes| primes.permutation(3).to_a.select { |p| p.sort.evenly_spaced? } }.map { |primes| primes.flatten.uniq }.uniq.reject(&:empty?).reject { |primes| primes.first == 1487 }.flatten.join
