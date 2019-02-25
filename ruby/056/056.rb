9077b9a1f71ac9d7219e130050ba411f99ce57dd
Sean Eshbaugh <seaneshbaugh@gmail.com>
2019-02-25 13:45:03 -0600

    Updated Ruby #56

--- ruby/056/056.rb
+++ ruby/056/056.rb
@@ -1,1 +1,1 @@
-puts (0..100).map { |a| (0..100).map { |b| (a**b).to_s.split('').map { |d| d.to_i }.inject(:+) } }.flatten.max
+puts (0..100).map { |a| (0..100).map { |b| (a**b).to_s.split('').map(&:to_i).inject(:+) } }.flatten.max
