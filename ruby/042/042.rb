b08174f2997b93c4ae9b4ce34f92dd4b0128350e
Sean Eshbaugh <seaneshbaugh@gmail.com>
2019-02-24 23:10:12 -0600

    Improved Ruby #42

--- ruby/042/042.rb
+++ ruby/042/042.rb
@@ -1,21 +1,17 @@
-class Array
-  def append_until(&block)
-    self.each do
-      result = block.yield(self)
-
-      self << result if result
-    end
+class Integer
+  def triangle
+    (self * (self + 1)) / 2
   end
 end
 
 contents = File.read(File.expand_path(File.join(__dir__, '..', '..', 'files', 'words.txt')))
 
-words = contents.split(',').map { |name| name.gsub('"', '') }.sort
+words = contents.split(',').map { |name| name.delete('"') }.sort
 
 word_values = words.map { |word| word.upcase.codepoints.map { |letter| letter - 64 }.inject(:+) }
 
 max = word_values.max
 
-triangle_numbers = [1].append_until { |t| (((t.length + 1) * (t.length + 2)) / 2) if (((t.length + 1) * (t.length + 2)) / 2) < max }
+triangle_numbers = (1..).lazy.map(&:triangle).take_while { |t| t < max }.to_a
 
 puts word_values.select { |word_value| triangle_numbers.include?(word_value) }.length
