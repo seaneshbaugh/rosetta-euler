1cf3118844fdb979f5bf724f1c8b3f96efb2b4a0
Sean Eshbaugh <seaneshbaugh@gmail.com>
2020-08-27 17:09:25 -0500

    Updated Clojure project files to use latest version

--- clojure/003/project.clj
+++ clojure/003/project.clj
@@ -1,7 +1,7 @@
 (defproject euler "0.0.1"
   :description "Project Euler #3 - Clojure"
   :url "https://github.com/seaneshbaugh/rosetta-euler/tree/master/clojure/003"
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]]
   :main ^:skip-aot euler.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
